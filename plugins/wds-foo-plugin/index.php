e08c4b6e6f0af9d41f257ec70e1d83b586c80f49
Greg Rickaby <gregrickaby@gmail.com>
2021-06-03 08:24:31 -0500

    create a fake plugin and theme

---
+++ plugins/wds-foo-plugin/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WDS Foo Plugin
+ * Plugin URI:  https://webdevstudios.com
+ * Description: A cool plugin that does stuff.
+ * Author:      WebDevStudios
+ * Author URI:  https://webdevstudios.com
+ * Version:     1.0.0
+ * Domain:      wds-foo-plugin
+ * License:     GPLv2
+ */
+
+function badFunctionName(){
+ $MYARRAY = array(
+     'key'=>'value'
+ )
+}
---
+++ themes/wd_s/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wd_s
+ */
+
+get_header(); ?>
+
+<?php
+
+function badFunctionName() {
+    return null;
+}
+
+?>
+
+<?php get_footer(); ?>
